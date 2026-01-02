fbbe42d18ac14ad8b13bee43988682c14cb9ad8c
Kyra Coyne <kyra.coyne@gmail.com>
2026-01-02 19:31:18 +0400

    Test display user search back to them

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItem, IonLabel, IonInput, IonButton } from '@ionic/angular/standalone';
 
@@ -6,12 +7,14 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, Io
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [ FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItem, IonLabel, IonInput, IonButton],
+  imports: [ CommonModule, FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItem, IonLabel, IonInput, IonButton],
 })
 export class HomePage {
   ingredients: string = '';
+  lastSearch: string = '';
   // Run this function when the user clicks the Search button
   onSearchClicked() {
+    this.lastSearch = this.ingredients;
     console.log('Ingredients entered:', this.ingredients);
   }
 }

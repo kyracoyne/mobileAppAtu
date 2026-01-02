5f78d6709be5e08659cd7282abef3a1382f69c8c
Kyra Coyne <kyra.coyne@gmail.com>
2026-01-02 17:57:47 +0400

    Fix project name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'recipeFinder',
+  appName: 'G00270766',
   webDir: 'www'
 };
 

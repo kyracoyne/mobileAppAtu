0a99ad7663e285f993fe33b31ee5fdeab7c6694e
Kyra Coyne <kyra.coyne@gmail.com>
2026-01-03 21:38:24 +0400

    Add recipe details back button and display ingredients and instructions using Ionic lists

--- src/app/recipe-details/recipe-details.page.ts
+++ src/app/recipe-details/recipe-details.page.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg, IonButtons, IonButton, IonIcon, IonList, IonItem, IonLabel, IonThumbnail } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
 import { SpoonacularService } from '../services/spoonacular';
+import { Router } from '@angular/router'; // added for back button
+import { arrowBackOutline } from 'ionicons/icons'; // added for back button
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.page.html',
   styleUrls: ['./recipe-details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonImg, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonImg, IonList, IonItem, IonLabel, IonThumbnail, CommonModule, FormsModule]
 })
 export class RecipeDetailsPage implements OnInit {
   recipeId: number = 0;
   recipe: any = null;
   errorMsg: string = '';
+  arrowBackOutline = arrowBackOutline;
   constructor(
     private route: ActivatedRoute,
-    private spoon: SpoonacularService
+    private spoon: SpoonacularService,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -40,4 +44,13 @@ export class RecipeDetailsPage implements OnInit {
       });
   }
 
+  getIngredientImageUrl(imageFileName: string): string {
+    return `https://img.spoonacular.com/ingredients_100x100/${imageFileName}`;
+  }
+
+  // Maybe obvious, but method for back button to get back to home page from recipe 
+  goBackHome() {
+    this.router.navigate(['/home']);
+  }
+  
 }
